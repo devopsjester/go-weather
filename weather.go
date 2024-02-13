713fe7219fced3eb1c0ec957c42a5d1dd8096bb5
Assaf Stone <1774556+devopsjester@users.noreply.github.com>
2024-02-13 19:55:20 +0000

    extract getting the zipcode from the main file

--- weather.go
+++ weather.go
@@ -5,20 +5,23 @@ import (
 	"fmt"
 )
 
-func main() {
-	// Define a string flag for zipcode with a default value and a short description.
+// getZipcode function to get the zipcode from command-line arguments
+func getZipcode() string {
 	zipcodePtr := flag.String("zipcode", "", "A five digit zipcode")
-
-	// Parse the command-line arguments
 	flag.Parse()
 
 	// Check if zipcode is a 5-digit number
 	if len(*zipcodePtr) != 5 {
 		fmt.Println("Please provide a 5-digit zipcode.")
-		return
+		os.Exit(1)
 	}
 
+	return *zipcodePtr
+}
+
+func main() {
+	zipcode := getZipcode()
+
 	// Print the zipcode
-	fmt.Println("The zipcode you entered is:", *zipcodePtr)
-	
+	fmt.Println("The zipcode you entered is:", zipcode)
 }
